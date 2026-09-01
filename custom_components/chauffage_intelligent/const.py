be64f3d501708fc806f1935f96e4cbe3cdc6af3f
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 14:38:03 +0200

    Update const.py

--- custom_components/chauffage_intelligent/const.py
+++ custom_components/chauffage_intelligent/const.py
@@ -7,3 +7,4 @@ CONF_TEMP_EXT = "Température extérieur"
 CONF_TEMP_INT = "Température intérieur"
 CONF_PLANNING = "Planning de la pièce utilisé"
 CONF_CLIMATE = "Thermostat de la pièce"
+CONF_DERIVE = "derive
