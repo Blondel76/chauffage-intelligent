68e45a7b4cb867b041866f87a7fbda01c8820520
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 14:40:03 +0200

    Update CONF_DERIVE to include sensor description

--- custom_components/chauffage_intelligent/const.py
+++ custom_components/chauffage_intelligent/const.py
@@ -7,4 +7,4 @@ CONF_TEMP_EXT = "Température extérieur"
 CONF_TEMP_INT = "Température intérieur"
 CONF_PLANNING = "Planning de la pièce utilisé"
 CONF_CLIMATE = "Thermostat de la pièce"
-CONF_DERIVE = "derive
+CONF_DERIVE = "derive capteur de température"
