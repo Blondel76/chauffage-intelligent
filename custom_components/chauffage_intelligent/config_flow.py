d88c09131f9430a98671377eb3f1befe81926880
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 12:51:22 +0200

    Update config_flow.py

--- custom_components/chauffage_intelligent/config_flow.py
+++ custom_components/chauffage_intelligent/config_flow.py
@@ -1,11 +1,21 @@
 """Configuration flow for Chauffage Intelligent."""
 
+from typing import Any
+
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
+from homeassistant.helpers import selector
 
-from .const import DOMAIN
+from .const import (
+    CONF_COEFFICIENT,
+    CONF_CONSIGNE,
+    CONF_ETAT,
+    CONF_PLANNING,
+    CONF_TEMP_EXT,
+    CONF_TEMP_INT,
+    DOMAIN,
+)
 
 
 class ChauffageIntelligentConfigFlow(
@@ -16,22 +26,59 @@ class ChauffageIntelligentConfigFlow(
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    async def async_step_user(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ):
+        """Handle the initial setup."""
 
         if user_input is not None:
             return self.async_create_entry(
-                title=user_input[CONF_NAME],
+                title="Chauffage Intelligent",
                 data=user_input,
             )
 
-        schema = vol.Schema(
+        data_schema = vol.Schema(
             {
-                vol.Required(CONF_NAME, default="Chauffage Intelligent"): str,
+                vol.Required(CONF_TEMP_EXT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="sensor",
+                    )
+                ),
+
+                vol.Required(CONF_TEMP_INT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="sensor",
+                    )
+                ),
+
+                vol.Required(CONF_CONSIGNE): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="climate",
+                    )
+                ),
+
+                vol.Required(CONF_COEFFICIENT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="input_number",
+                    )
+                ),
+
+                vol.Required(CONF_PLANNING): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="input_text",
+                    )
+                ),
+
+                vol.Required(CONF_ETAT): selector.EntitySelector(
+                    selector.EntitySelectorConfig(
+                        domain="climate",
+                    )
+                ),
             }
         )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=data_schema,
         )
