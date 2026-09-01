8f8b02a2a63be7a6eac1861fececb62d550e751a
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 22:41:43 +0200

    Refactor setup and unload functions in __init__.py

--- custom_components/chauffage_intelligent/__init__.py
+++ custom_components/chauffage_intelligent/__init__.py
@@ -1,5 +1,7 @@
 """Chauffage Intelligent."""
 
+from __future__ import annotations
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
@@ -11,6 +13,9 @@ async def async_setup_entry(
     entry: ConfigEntry,
 ) -> bool:
     """Set up Chauffage Intelligent from a config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
@@ -26,7 +31,12 @@ async def async_unload_entry(
 ) -> bool:
     """Unload Chauffage Intelligent."""
 
-    return await hass.config_entries.async_unload_platforms(
+    unloaded = await hass.config_entries.async_unload_platforms(
         entry,
         ["sensor", "number"],
     )
+
+    if unloaded:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unloaded
