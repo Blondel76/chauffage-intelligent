ef470b6a52bcca60eda9dbdfc1e560df0128e0b5
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 13:42:06 +0200

    Rename CONF_ROOM to CONF_AREA in const.py

--- custom_components/chauffage_intelligent/const.py
+++ custom_components/chauffage_intelligent/const.py
@@ -2,8 +2,7 @@
 
 DOMAIN = "chauffage_intelligent"
 
-CONF_ROOM = "room"
-
+CONF_AREA = "area"
 CONF_TEMP_EXT = "Température extérieur"
 CONF_TEMP_INT = "Température intérieur"
 CONF_PLANNING = "Planning de la pièce utilisé"
