c843aa912be6bc382aec1f9055264e9c30e603cd
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 12:54:31 +0200

    Refactor async_setup and add entry setup functions

--- custom_components/chauffage_intelligent/__init__.py
+++ custom_components/chauffage_intelligent/__init__.py
@@ -1,10 +1,40 @@
 """Intégration Chauffage Intelligent."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Configurer l'intégration Chauffage Intelligent."""
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    """Set up the Chauffage Intelligent integration."""
     return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up Chauffage Intelligent from a config entry."""
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["sensor"],
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload Chauffage Intelligent."""
+
+    return await hass.config_entries.async_unload_platforms(
+        entry,
+        ["sensor"],
+    )
