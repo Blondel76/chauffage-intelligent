1a5631116b4710f2a813db1ebe9b3b79ab66fe38
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-01 12:45:03 +0200

    Define constants for Chauffage Intelligent
    Added configuration constants for Chauffage Intelligent.

--- custom_components/chauffage_intelligent/const.py
+++ custom_components/chauffage_intelligent/const.py
@@ -1,1 +1,10 @@
+"""Constants for Chauffage Intelligent."""
+
 DOMAIN = "chauffage_intelligent"
+
+CONF_TEMP_EXT = "temp_ext"
+CONF_TEMP_INT = "temp_int"
+CONF_CONSIGNE = "consigne"
+CONF_COEFFICIENT = "coefficient"
+CONF_PLANNING = "planning"
+CONF_ETAT = "etat"
