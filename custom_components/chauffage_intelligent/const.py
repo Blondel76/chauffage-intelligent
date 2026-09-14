7bf1d4b853c565f542a56b555d4eaddbba4dda13
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-09-14 07:36:29 +0200

    Refactor constants with French translations and cleanup
    Updated configuration constants to use French terms and removed unused constants.

--- custom_components/chauffage_intelligent/const.py
+++ custom_components/chauffage_intelligent/const.py
@@ -9,28 +9,19 @@ DOMAIN = "chauffage_intelligent"
 ENTRY_TYPE = "entry_type"
 ENTRY_TYPE_CENTRAL = "central"
 ENTRY_TYPE_ROOM = "room"
-ENTRY_TYPE_GROUP = "group"
 CENTRAL_UNIQUE_ID = "chauffage_intelligent_central"
 
 CONF_MODE_SELECTOR = "mode_selector"
-CONF_HEATING_TYPE = "heating_type"
-HEATING_TYPE_GAS = "gaz"
-HEATING_TYPE_ELECTRIC = "electrique"
 
 CONF_AREA = "area"
-CONF_TEMP_EXT = "temp_ext"
-CONF_TEMP_INT = "temp_int"
-CONF_CLIMATE = "climate_entity"
-CONF_DOOR_SENSOR = "door_sensor"
-CONF_HEATER_ENTITY = "heater_entity"
+CONF_TEMP_EXT = "Température extérieur"
+CONF_TEMP_INT = "Température intérieur"
+CONF_CLIMATE = "Thermostat de la pièce"
+CONF_DOOR_SENSOR = "Capteur de porte/fenêtre (optionnel)"
+CONF_HEATER_ENTITY = "Vanne du radiateur"
 
 CONF_MODE_PLANNINGS = "mode_plannings"
 DEFAULT_PLANNING = "12:00|eco"
-
-CONF_GROUP_NAME = "group_name"
-CONF_GROUP_AREAS = "group_areas"
-CONF_GROUP_THRESHOLD = "group_threshold"
-DEFAULT_GROUP_THRESHOLD = 0.2
 
 COEFFICIENT_MIN = 10.0
 COEFFICIENT_MAX = 60.0
